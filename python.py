0782004f5dda1213fa9bcbf303fbde772f28198b
jhansirani-s <jhansirani.s@trp.srmtrichy.edu.in>
2026-02-20 15:28:29 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,13 @@
+# ==========================================
+# Assignment: FODSA
+# Student Name:
+# Register Number:
+# ==========================================
+
+def solve():
+    # Write your logic here
+    pass
+
+
+if __name__ == "__main__":
+    solve()
